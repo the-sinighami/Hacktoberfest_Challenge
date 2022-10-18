8540f41c7db4013c55644c395b087fac09b2347b
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2022-10-18 12:37:56 +0530

    Add files via upload

---
+++ strings.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int main()
+{
+   // string str;
+   // cin>>str;
+   // cout<<str<<endl;
+   // 
+   // string str1(5,'n');
+   // cout<<str1<<endl;
+//
+   // Input
+//
+   // string str2;
+   // cin>>str2;
+   // cout<<str2;
+//
+
+    //getline() function is used to take input of string having spaces
+
+  //  string str3;
+  //  getline(cin,str3);
+  //  cout<<str3<<endl;
+//
+ 
+    string s1 = "fam";
+    string s2 = "ily";
+
+    //s1.append(s2);
+    cout<<s1+s2;
+
+
+
+
+
+
+    return 0;
+}
